082663d9bac8353d7bcbe84c09d675a8ec53f809
Anjali Raja <anjaliraja1406@gmail.com>
2024-11-09 00:05:32 +0530

    add codes

---
+++ sum_diff.java
@@ -0,0 +1,14 @@
+public class sum_diff
+{
+    public static void main (String[] args)
+    {
+       int a=90;
+       int b=76;
+       int sum=a+b;
+       int diff=a-b;
+       System.out.println(sum);
+       System.out.println(diff);
+
+
+    }
+}
---
+++ variables.java
@@ -0,0 +1,13 @@
+public class variables
+{
+    public static void main (String[] args)
+    {
+        String name="Anjali raja";
+        int age=48;
+        double price=65.89;
+        int a=8;
+        System.out.println(name);
+        System.out.println(price);
+        System.out.println(a);
+    }
+}
